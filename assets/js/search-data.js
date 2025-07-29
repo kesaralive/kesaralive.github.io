e6600232a005d95b870afb99fbd7ec47696229df
kesaralive <90370604+kesaralive@users.noreply.github.com>
2025-07-29 19:44:25 +0000

    Deploying to gh-pages from @ kesaralive/kesaralive.github.io@2aa6b1643f8d966c4ee4fb1f2e1a94df1a897d5b 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -501,7 +501,7 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%68%65%6C%6C%6F@%6B%65%73%61%72%61%6C%69%76%65.%63%6F%6D", "_blank");
         },
       },{
         id: 'social-inspire',
